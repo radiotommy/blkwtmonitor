a389cc050ca4c5c40199eb1865444865f83b9d88
Tommy <radiotommy@gmail.com>
2018-11-05 23:46:17 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # blkwtmonitor
-count block device sector write request
+Read and log the block device sector write requests from sys/block/DEV_NAME/stat to roughly measure a sdcard usage
+
+See https://www.kernel.org/doc/Documentation/block/stat.txt for details.
+
+# note
+The sector is not the physical sector of the block device.
+
+# usage
+python blkwtmon.py mmcblk 60 test.csv
+
+
---
+++ blkwtmon.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import os
+import time
+import sys
+from datetime import datetime
+
+def read_state (blkdev) :
+    cols = ('read_reqs', 'read_merges', 'read_sectors', 'read_ticks', 'write_reqs', 'write_merges', 'write_sectors', 'write_ticks') 
+    statFileName = os.path.join('/sys/block', blkdev, 'stat')
+    f = open(statFileName, 'r')
+    stat = f.read().split()
+    f.close()
+
+    return dict(zip(cols, stat))
+
+def log_sector_writes (blkdev, logname, last_reading):
+    stat = read_state(devname)
+    wt_sects = int(stat['write_sectors'])
+    delt = wt_sects - last_reading
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    rec = '%s,%d,%d' % (date, wt_sects, delt)
+    f = open(logname, 'a')
+    f.write(rec + '\n')
+    f.close()
+    return rec, wt_sects
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 4 :
+        print '''
+===============================================================================================
+Usage:
+    python blkwtmon.py BLOCK_DEVICE_NAME INTERVAL LOG_NAME
+
+    BLOCK_DEVICE_NAME:  the block device name monitor on
+    INTERVAL:           check interval in seconds
+    LOG_NAME:           where to save the log
+
+Example:
+    python blkwtmon mmcblk 60 test.csv
+    will read sector write request counter to mmcblk every minute and save to test.csv
+===============================================================================================
+'''
+    else :
+        devname = sys.argv[1]
+        interval = int(sys.argv[2])
+        logname = sys.argv[3]
+
+        print 'Monitor block device %s sector writing every %d seconds' % (devname, interval)
+        stat = read_state(devname)
+        last_reading = int(stat['write_sectors'])
+
+        while(1) :
+            time.sleep(interval)
+            stat = read_state(devname)
+            rec, wt_sects = log_sector_writes(devname, logname, last_reading)
+
+            print rec
+            last_reading = wt_sects
+
